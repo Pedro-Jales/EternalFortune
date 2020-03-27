5fde4c9e2531826c41d06b90fa899f2c58b8db3c
ivebeentohell <devilmancrybaby350@gmail.com>
2020-03-27 01:20:18 +0100

    initial commit

---
+++ src/main/java/com/ivebeentohell/mineinabyss/eternalfortune/EternalFortunePlugin.java
@@ -0,0 +1,12 @@
+package com.ivebeentohell.mineinabyss.eternalfortune;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EternalFortunePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
